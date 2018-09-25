6ff2da7283f22bf723b5b15e8edde54720091988
Kevin Vitale <kevinvitale@gmail.com>
2018-09-25 13:25:11 -0400

    [Swift] adds ability to set display mode

--- displix.swift
+++ displix.swift
@@ -29,11 +29,9 @@ public struct Display
     }
 
     /// Available display modes.
-    public func modes(showLowResolutions lowRes: Bool = true) -> [CGDisplayMode] {
-        let options = [
-            kCGDisplayShowDuplicateLowResolutionModes : lowRes ? kCFBooleanTrue : kCFBooleanFalse
-        ] as CFDictionary?
-        return CGDisplayCopyAllDisplayModes(id, options) as? [CGDisplayMode] ?? []
+    public func modes(showingLowResolutions lowRes: Bool) -> [CGDisplayMode] {
+        let options = [ kCGDisplayShowDuplicateLowResolutionModes : kCFBooleanTrue ] as CFDictionary?
+        return CGDisplayCopyAllDisplayModes(id, lowRes ? options : nil) as? [CGDisplayMode] ?? []
     }
 
     /**
@@ -54,14 +52,31 @@ public struct Display
     /**
      */
     func set(mode: CGDisplayMode) throws {
+        var err: CGError
+        var config: CGDisplayConfigRef? = nil
+
+        err = CGBeginDisplayConfiguration(&config);
+        guard err == .success else {
+            throw Error.error(err)
+        }
+
+        err = CGConfigureDisplayWithDisplayMode(config, id, mode, nil);
+        guard err == .success else {
+            throw Error.error(err)
+        }
+
+        err = CGCompleteDisplayConfiguration(config, .forSession);
+        guard err == .success else {
+            throw Error.error(err)
+        }
     }
 }
 
 /**
  */
 extension Display {
-    func printModes(showingLowResolutions lowRes: Bool = true) {
-        let displayModes =  modes(showLowResolutions: lowRes)
+    func printModes(showingLowResolutions lowRes: Bool) {
+        let displayModes =  modes(showingLowResolutions: lowRes)
         print("\tID:\t\(id)")
         print("\tModes:\t\(displayModes.count)")
         print("\t-----\t-----\t------");
@@ -116,9 +131,57 @@ extension Display {
     }
 }
 
-let displays = try! Display.all(.online)
-for (idx, display) in displays.enumerated() {
-    print("DISPLAY: \(idx)")
-    display.printModes()
-    print()
+public struct Settings {
+    public var showLowRes: Bool = false
+    public var display: Display = .main
+    public var modeIndex: Int   = -1
+
+    public static func parseCommandLineArguments(displays: [Display]) -> Settings {
+        var settings = Settings()
+
+        var opt: Int32 = 0
+        repeat {
+            opt = getopt(CommandLine.argc, CommandLine.unsafeArgv, "d:m:a")
+            guard opt >= 0 else { break }
+
+            switch Unicode.Scalar(Int(opt)) {
+                case "a": 
+                    settings.showLowRes = true
+                case "d": 
+                    let idx = Int(atoi(optarg))
+                    if idx < displays.count {
+                        settings.display = displays[idx]
+                    }
+                case "m": 
+                    settings.modeIndex = Int(atoi(optarg))
+                default: ()
+            }
+        } while opt != -1
+
+        return settings
+    }
 }
+
+let displays = try! Display.all(.online)
+let settings: Settings = .parseCommandLineArguments(displays: displays)
+
+guard settings.modeIndex < 0 else {
+    let modes = settings.display.modes(showingLowResolutions: settings.showLowRes)
+    guard settings.modeIndex < modes.count else {
+        settings.display.printModes(showingLowResolutions: settings.showLowRes)
+        exit(0)
+    }
+    let mode = modes[settings.modeIndex]
+    print("W: \(mode.width); H: \(mode.height)")
+    do {
+        try settings.display.set(mode: mode)
+    } catch {
+        print(error)
+    }
+    exit(0)
+}
+
+displays.forEach {
+    $0.printModes(showingLowResolutions: settings.showLowRes)
+}
+
